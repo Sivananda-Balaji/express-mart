c4d47a2ac33b180a19a6738b36a3e3f56ed2ea15
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-07 00:49:29 +0530

    add auth and address route in app.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const authRouter = require("./routes/auth.router");
+const addressRouter = require("./routes/address.router");
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1", authRouter);
+app.use("/api/v1/addresses", addressRouter);
 
 module.exports = app;
