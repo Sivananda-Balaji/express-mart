2bd0bd6ff3cc8c56b29f69fad7b24154f8f40eb7
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-10 18:08:41 +0530

    Add products endpoint to the app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRouter = require("./routes/auth.router");
 const addressRouter = require("./routes/address.router");
+const productRouter = require("./routes/product.router");
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use("/api/v1", authRouter);
 app.use("/api/v1/addresses", addressRouter);
+app.use("/api/v1/products", productRouter);
 
 module.exports = app;
