89310eb00afa6eec3f775d3ed52637d75817f55d
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-12 01:14:30 +0530

    Add updateProduct and deleteProduct controller function. This controller functions allows the user to update and delete existing products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -59,4 +59,69 @@ const getproduct = async (req, res) => {
   }
 };
 
-module.exports = { createProduct, getCategories, getproduct };
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const {
+      name,
+      category,
+      price,
+      description,
+      manufacturer,
+      availableItems,
+      imageUrl,
+    } = req.body;
+    const updatedProduct = await Product.findOneAndUpdate(
+      { _id: id },
+      {
+        name,
+        category,
+        price,
+        description,
+        manufacturer,
+        availableItems,
+        imageUrl,
+        updatedAt: Date.now(),
+      },
+      { new: true, projection: { __v: 0 } }
+    );
+    if (!updatedProduct) {
+      return res.status(404).send({
+        message: `No Product found for ID - ${id}!`,
+      });
+    }
+    res.send(updatedProduct);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      message: "internal server error",
+    });
+  }
+};
+
+const deleteproduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete({ _id: id });
+    if (!deletedProduct) {
+      return res.status(404).send({
+        message: `No Product found for ID - ${id}!`,
+      });
+    }
+    res.send({ message: `Product with ID - ${id} deleted successfully!` });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      message: "something went wrong",
+    });
+  }
+};
+
+module.exports = {
+  createProduct,
+  getCategories,
+  getproduct,
+  updateProduct,
+  deleteproduct,
+};
