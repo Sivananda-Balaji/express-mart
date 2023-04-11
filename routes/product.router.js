06c5be8adbeb2febc0aa9ce9c3661283a22aed71
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-11 14:59:16 +0530

    Add route to product and product categories

--- routes/product.router.js
+++ routes/product.router.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const { verifyToken, isAdmin } = require("../middleware/auth");
-const { createProduct } = require("../controllers/product.controller");
+const {
+  createProduct,
+  getCategories,
+  getproduct,
+} = require("../controllers/product.controller");
 
 const router = express.Router();
 
 router.route("/").post([verifyToken, isAdmin], createProduct);
+router.route("/categories").get(getCategories);
+router.route("/:id").get(getproduct);
 
 module.exports = router;
