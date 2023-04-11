f4a6be8cab13fc009bb0123313c80448efd7e784
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-12 01:24:55 +0530

    Add update and delete routes for the products

--- routes/product.router.js
+++ routes/product.router.js
@@ -4,12 +4,18 @@ const {
   createProduct,
   getCategories,
   getproduct,
+  updateProduct,
+  deleteproduct,
 } = require("../controllers/product.controller");
 
 const router = express.Router();
 
 router.route("/").post([verifyToken, isAdmin], createProduct);
 router.route("/categories").get(getCategories);
-router.route("/:id").get(getproduct);
+router
+  .route("/:id")
+  .get(getproduct)
+  .put([verifyToken, isAdmin], updateProduct)
+  .delete([verifyToken, isAdmin], deleteproduct);
 
 module.exports = router;
