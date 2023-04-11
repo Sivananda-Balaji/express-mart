967e8c7dd55cc6a8df1759e7eb6165605065da06
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-11 14:57:57 +0530

    Add controller function to get product categories and product via id

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,7 @@ const Product = require("../models/product.model");
 
 const createProduct = async (req, res) => {
   try {
-    let {
+    const {
       name,
       category,
       price,
@@ -32,4 +32,31 @@ const createProduct = async (req, res) => {
   }
 };
 
-module.exports = { createProduct };
+const getCategories = async (req, res) => {
+  try {
+    const categories = await Product.distinct("category");
+    res.send(categories);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: "something went wrong" });
+  }
+};
+
+const getproduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById({ _id: id });
+    if (!product) {
+      return res
+        .status(404)
+        .send({ message: `No Product found for ID - ${id}!` });
+    }
+    const productResponse = product.toObject({ versionKey: false });
+    res.send(productResponse);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: "something went wrong" });
+  }
+};
+
+module.exports = { createProduct, getCategories, getproduct };
