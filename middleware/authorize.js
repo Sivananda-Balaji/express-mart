ab98b45e01aa5d55cd22f80faeaa52351550c099
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-13 18:00:11 +0530

    Add isAdmin middleware function to authorize the admin user

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,5 +1,23 @@
 const User = require("../models/user.model");
 
+const isAdmin = async (req, res, next) => {
+  try {
+    const adminUser = await User.findOne({ _id: req._id });
+    if (adminUser && adminUser.role === "ADMIN") {
+      next();
+    } else {
+      return res.status(403).send({
+        message: "You are not authorised to access this endpoint!",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      message: "something went wrong",
+    });
+  }
+};
+
 const isUser = async (req, res, next) => {
   try {
     const user = await User.findOne({ _id: req._id });
@@ -18,4 +36,4 @@ const isUser = async (req, res, next) => {
   }
 };
 
-module.exports = { isUser };
+module.exports = { isUser, isAdmin };
