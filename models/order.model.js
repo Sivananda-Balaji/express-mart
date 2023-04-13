2e24198c331c530c6cead5ea39839bbbf6c36dc2
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-13 14:16:39 +0530

    Add id increment logic for orders

--- models/order.model.js
+++ models/order.model.js
@@ -1,25 +1,38 @@
 const mongoose = require("mongoose");
 
 const orderSchema = mongoose.Schema({
+  _id: {
+    type: Number,
+  },
   amount: {
-    type: mongoose.Schema.Types.Double,
+    type: Number,
   },
   orderDate: {
-    type: String,
+    type: Date,
     default: Date.now(),
   },
   user: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Number,
     ref: "User",
   },
   product: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Number,
     ref: "Product",
   },
   shippingAddress: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Number,
     ref: "Address",
   },
+});
+
+orderSchema.pre("save", async function (next) {
+  const doc = this;
+  if (doc.isNew) {
+    const lastOrder = await Order.findOne().sort({ _id: -1 });
+    const newId = (lastOrder && lastOrder._id + 1) || 4000;
+    doc._id = newId;
+  }
+  next();
 });
 
 const Order = mongoose.model("Order", orderSchema);
