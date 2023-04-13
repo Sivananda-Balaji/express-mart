c83dd1cba2b912bef32ace4ee8b1425186dae93b
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-13 14:29:12 +0530

    Add orders endpoint to the app. This endpoint redirect order related request to order router.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authRouter = require("./routes/auth.router");
 const addressRouter = require("./routes/address.router");
 const productRouter = require("./routes/product.router");
+const orderRouter = require("./routes/order.router");
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use("/api/v1", authRouter);
 app.use("/api/v1/addresses", addressRouter);
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/orders", orderRouter);
 
 module.exports = app;
