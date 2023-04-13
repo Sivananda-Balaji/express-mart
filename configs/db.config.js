1e361841a9d40fb62eb996cb82eb62cd8eb27870
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-14 00:45:14 +0530

    Add DB_String to the project.

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   //Add DB-String to connect with DB
-  DB_URL: "",
+  DB_URL:
+    "mongodb+srv://Guest:0NFDy03BjieaYqI5@cluster-guest.niyjoyz.mongodb.net/express_mart_db?retryWrites=true&w=majority",
 };
